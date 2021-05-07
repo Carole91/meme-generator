f2673f072f0054bd0e973886a0f72e0f39c45ffd
Carole91 <75634638+Carole91@users.noreply.github.com>
2021-05-07 16:13:13 +0200

    new comit

---
+++ src/App.js
@@ -0,0 +1,112 @@
+import React, {
+  useState,
+  useEffect,
+  useRef
+} from 'react';
+import './App.css';
+import axios from "axios";
+import Draggable, {DraggableCore} from 'react-draggable'; // <DraggableCore>
+import domtoimage from 'dom-to-image';
+
+
+function App() {
+  let imageAreaRef = useRef(null);
+
+  const [templates, setTemplates] = useState(null)
+  const [firstTitle, setFirstTitle] = useState('');
+  const [secondTitle, setSecondTitle] = useState('');
+  const [randomNumber, setRandomNumber] = useState(0);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [uploadedImage, setUploadedImage] = useState(null);
+
+  const genRandomNumber = () => {
+    setUploadedImage(null);
+    setRandomNumber(Math.floor(Math.random() *100))
+  }
+
+  useEffect(async () => {
+    await axios.get('https://api.imgflip.com/get_memes')
+      .then(response => {
+        setTemplates(response.data.data.memes);
+        genRandomNumber();
+        console.log(response.data.data.memes)
+      });
+
+      imageAreaRef.current.focus();
+
+  }, []);
+
+  const onSelectFileHandler = event =>{
+    console.log(event.target.files[0]);
+    setSelectedFile(event.target.files[0])
+  }
+
+  const onUploadHandler = () => {
+    let src = URL.createObjectURL(selectedFile);
+    setUploadedImage(src);
+    console.log(src);
+  }
+
+  const exportImageHandler = () => {
+
+    console.log(`Image dom = ${imageAreaRef.current}`);
+    
+    domtoimage.toJpeg(document.getElementsByClassName('meme-contents')[0], { quality: 0.95 })
+    .then(function (dataUrl) {
+        var link = document.createElement('a');
+        link.download = 'my-image-name.jpeg';
+        link.href = dataUrl;
+        link.click();
+    });
+  }
+
+  return ( 
+    <div>
+      <h1>Meme Generator</h1>
+      <br/>
+      <div className="input-area">
+        <div>
+          <label for = "first-title">First Title</label>
+          <input onChange={(e)=>setFirstTitle(e.target.value)} value={firstTitle} name="first-title" id="first-title" type="text"/>
+        </div>
+        <div>
+          <label for = "second-title">Second Title</label>
+          <input onChange={(e)=>setSecondTitle(e.target.value)} value={secondTitle} name="second-title" id="second-title" type="text"/>
+        </div>
+      </div>
+      { templates ?
+      <div>
+        <div ref={imageAreaRef} className="meme-contents">  
+          <img src={uploadedImage ? uploadedImage : templates[randomNumber].url} className="meme-contents__meme-image"/> 
+          <Draggable>
+            <div className="meme-contents__firstTitle">{firstTitle}</div>
+          </Draggable>
+          <Draggable>
+            <div className="meme-contents__secondTitle">{secondTitle}</div>
+          </Draggable>
+        </div>
+      
+        <div className="img-selections">
+          <button onClick={() => { setUploadedImage(null); setRandomNumber(randomNumber - 1)}}>Previous</button>
+          <button onClick={() => { setUploadedImage(null); setRandomNumber(randomNumber + 1)}}>Next </button>
+          <button onClick={genRandomNumber}>Pick random</button>
+        </div>
+        <hr/>
+        <div className="upload-area">
+          <input type="file" name="file" onChange={onSelectFileHandler}/>
+          <button type="button" className="" onClick={onUploadHandler}>Upload</button> 
+          {/* <input type= "remove" name="remove" */}
+        </div>
+        <hr/>
+        <div className="export-image-area">
+          <button onClick={exportImageHandler}>export image</button>
+        </div>
+      </div>
+      : <p>loading image</p>
+      }
+      
+    </div>
+  );
+}
+
+export default App;
